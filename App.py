1a5e398e628f1d2d83e43df27560db7dff3015e8
Elisee Djapa <eliseedjapa@gmail.com>
2022-03-01 11:44:56 -0500

    Progress made on PAntry DB

--- App.py
+++ App.py
@@ -1,30 +1,66 @@
-import FoodPantryDB
+import database_pantry
+from datetime import datetime
 
-MENU_PROMPT = '''
+
+MENU_PROMPT = """ ---Food Pantry ---
+
 Please Choose one of these options:
 
 1) Add a new product
 2) See all my products
 3) Find a product by name
-4) See which is the home fav item
-5) Exit Process
+4) Find a product by expiration date
+5) See which is the home fav item
+6) Exit Process
 
-What is your selection: ? '''
+What is your selection?: """
+
 
 def menu():
-    connection = FoodPantryDB.connect()
-    FoodPantryDB.create_tables(connection)
+    connection = database_pantry.connect()
+    database_pantry.create_tables(connection)
 
-    while (user_input :=  input(MENU_PROMPT)) != "5":
+    while (user_input := input(MENU_PROMPT)) != "6":
         if user_input == "1":
-            pass
+            product_name = input("Enter the name of the household product: ")
+            quantity_number = int(input("Enter the number of household product: "))
+            dick = str(input("Enter date (yyyy-mm-dd): "))
+            expiration_date = datetime.strptime(dick, "%Y-%m-%d").date()
+            house_rating = int(input("Enter your rating score for this product (0-100): "))
+
+            database_pantry.add_food_pantry(connection, product_name, quantity_number, expiration_date, house_rating)
+
         elif user_input == "2":
-            pass
+            Food_Manager = database_pantry.get_all_pantry(connection)
+            for food in Food_Manager:
+                print(f"{food[1]}  ({food[2]}) ({food[3]}) - {food[4]}/100")
+
         elif user_input == "3":
-            pass
+
+            product_name = input("Enter a household product name to find: ")
+            Food_Manager = database_pantry.get_all_pantry_by_name(connection, product_name)
+            
+            for food in Food_Manager:
+                print(f"{food[1]}  ({food[2]})  ({food[3]}) - {food[4]}/100")
+
         elif user_input == "4":
-            pass
-        else:   
+            
+            expiration_date = str(input('Enter the expiration date: ')) 
+            Food_Manager = database_pantry.get_all_pantry_by_expiration_date(connection, expiration_date)
+            
+            for time in Food_Manager:
+                print(f"{time[1]}  ({time[2]})  ({time[3]}) - {time[4]}/100")
+                
+        elif user_input == "5":
+                
+            product_name = input("Enter product to find: ")
+            house_rating = database_pantry.get_fav_food(connection, product_name)
+            
+            print("The favorite household item for {product_name} is:  {house_rating[4]}")
+                        
+        else:
             print("Invalid input, try again!...ahahaha")
-                   
+  
+
+
 menu()
---
+++ database_pantry.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+CREATE_FOOD_PANTRY_MANAGER_TABLE = "CREATE TABLE IF NOT EXISTS Food_Pantry_Manager (product_id INTEGER PRIMARY KEY,  product_name TEXT, quantity_number INTEGER, expiration_date DATE, house_rating INTEGER ); "
+
+INSERT_FOOD_PANTRY = "INSERT INTO Food_Pantry_Manager (product_name, quantity_number, expiration_date, house_rating)  VALUES (?, ?, ?, ?);"
+
+GET_ALL_PANTRY = "SELECT * FROM Food_Pantry_Manager;"
+
+GET_ALL_PANTRY_BY_NAME = "SELECT * FROM Food_Pantry_Manager WHERE product_name = ?;"
+
+GET_ALL_PANTRY_BY_DATE = "SELECT * FROM Food_Pantry_Manager WHERE expiration_date = ?;"
+
+# GET_FAV_FOOD = """ 
+# SELECT * FROM Food_Pantry_Manager
+# WHERE house_rating = (SELECT MAX(house_rating)) 
+# FROM food_Pantry_Manager
+# """
+
+
+
+
+def connect():
+    return sqlite3.connect('pantry.db')
+
+
+def create_tables(connection):
+    with connection:
+        connection.execute(CREATE_FOOD_PANTRY_MANAGER_TABLE)
+
+
+def add_food_pantry(connection, product_name, quantity_number, expiration_date, house_rating):
+    with connection:
+        connection.execute(INSERT_FOOD_PANTRY, (product_name, quantity_number, expiration_date, house_rating))
+
+
+def get_all_pantry(connection):
+    with connection:
+        return connection.execute(GET_ALL_PANTRY).fetchall()
+
+
+def get_all_pantry_by_name(connection, product_name):
+    with connection:
+        return connection.execute(GET_ALL_PANTRY_BY_NAME, (product_name,)).fetchall()
+
+
+def get_all_pantry_by_expiration_date(connection, expiration_date):
+    with connection:
+        return connection.execute(GET_ALL_PANTRY_BY_DATE, (expiration_date,)).fetchall()
+
+
+def get_fav_food(connection, house_rating):
+    with connection:
+        return connection.execute(GET_FAV_FOOD, (house_rating,)).fetchone()
+
+
